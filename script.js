dae0ff19e91774f937e30c3769674afbdf0df080
Bre88D <ghitobres@gmail.com>
2022-10-18 18:31:06 -0500

    firts commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function register(){
+
+
+      let user = {
+        name: document.getElementById("name").value,
+        lastName: document.getElementById("lastName").value,
+        age: document.getElementById("age").value,
+        job: document.getElementById("job").value,
+        rh: document.getElementById("rh").value,
+        docu: document.getElementById("docu").value,
+        phone: document.getElementById("phone").value,
+         
+      }
+      return 
+
+}
+
+
+ 
+
+
+
